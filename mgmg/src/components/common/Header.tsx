1034b430110423fe1f2964e8fff9663bef4ac9b6
ByeongjaeJeon <jbjeon0113@gmail.com>
2023-08-31 01:27:00 +0900

    style: add link to home at header

--- mgmg/src/components/common/Header.tsx
+++ mgmg/src/components/common/Header.tsx
@@ -1,12 +1,17 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
   return (
     <header className={cx("header")}>
-      <h1 className={cx("title")}>MGMG</h1>
+      <h1 className={cx("title")}>
+        <Link to="/" className={cx("link_to_home")}>
+          MGMG
+        </Link>
+      </h1>
     </header>
   );
 };
